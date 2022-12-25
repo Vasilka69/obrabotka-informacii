66b83a562b34ef935acf0e533160fbcbd24aa492
Vasilka69 <114226684+Vasilka69@users.noreply.github.com>
2022-12-25 23:35:21 +0500

    Хэминг

--- 2 лаба Помехоустойчивое кодирование (Алгоритм Хэмминга)/Laba2/main.py
+++ 2 лаба Помехоустойчивое кодирование (Алгоритм Хэмминга)/Laba2/main.py
@@ -1,4 +1,6 @@
+
 def zip(inp):
+    print(f'---Кодер---')
     print(f'Входная последовательность: {inp}')
     print(f'Длина последовательности: {len(inp)}')
     powers = []
@@ -30,9 +32,49 @@ def zip(inp):
         output = output[:i - 1] + str(counter % 2) + output[i:]
 
     print(f'Выходная последовательность: {output}')
+    return output
+
+def unzip(inp :str):
+    print(f'---Декодер---')
+    print(f'Входная последовательность: {inp}')
+    powers = []
+    pow = 0
+    while 2 ** pow <= len(inp):
+        powers.append(2 ** pow)
+        pow += 1
+    print(f'Степени: {powers}')
+
+    errors = []
+    for power in powers:
+        #print(f'Power = {power}')
+        ctrlBit = int(inp[power-1])
+        summ = int(0)
+        bit = int(power - 1)
+        while bit <= (len(inp) - power):
+            for i in range(power):
+                summ += int(inp[bit])
+                bit += 1
+            bit += power
+        summ %= 2
+        if summ != ctrlBit:
+            errors.append(power)
+    print(f'Контрольные биты с ошибкой: {errors}')
+
+    # Исправление
+    index = sum(errors) - 1
+    print(f'Индекс бита с ошибкой: {sum(errors)}')
+
+    output = inp[:index] + str(0) + inp[index + 1:] if inp[index] == '1' else inp[:index + 1] + str(1) + inp[index:]
+    print(f'Исправленная последовательность: {output}')
+
+
 
 
 if __name__ == '__main__':
-    inp = str(10110111001)
-    zip(inp)
+    inp = str('10110111001')
+    code = zip(inp)
+    print()
+    code = str('001001100111001')
+    code = str('001001101111001') # С ошибкой
+    unzip(code)
 
