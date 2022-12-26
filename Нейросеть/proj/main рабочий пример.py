da09104ceb67402b65b9f75dba21057da04d0640
Vasilka69 <114226684+Vasilka69@users.noreply.github.com>
2022-12-26 20:29:30 +0500

    Нейросеть пример

---
+++ Нейросеть/proj/main рабочий пример.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab as pl
+from tensorflow import keras
+#%matplotlib inline
+#from tensorflow.keras.datasets import fashion_dataset
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras import utils
+
+
+def main():
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+    index = 0
+
+    plt.figure()
+    plt.imshow(x_train[index]) # 0 - 59999
+    plt.colorbar()
+    plt.grid(False)
+
+    # Нормализуем
+    x_train = x_train / 255
+    x_test = x_test / 255
+    ###
+    '''
+    plt.figure(figsize=(10, 10))
+    for i in range(25):
+        plt.subplot(5, 5, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(x_train[i], cmap=plt.cm.binary)
+        pl.xlabel(class_names[y_train[i]])
+    plt.show()
+    '''
+    # Нейросеть
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation="relu"),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+
+    # Компиляция модели
+    model.compile(optimizer=keras.optimizers.SGD(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+
+    # Обучение
+    model.fit(x_train, y_train, epochs=10)
+
+    # Проверка точности предсказаний
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+    print('Test accuracy: ', test_acc)
+
+    # Предсказание
+    predictions = model.predict(x_train)
+    dictinary = dict(zip(class_names, predictions[index]))
+    print(f'Список классов и их вероятности')
+    for key in dictinary.keys():
+        if dictinary[key] == max(dictinary.values()):
+            print('>', end='')
+        print(f'{key} : {dictinary[key]}')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
