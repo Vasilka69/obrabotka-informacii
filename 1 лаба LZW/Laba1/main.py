b798c921f48b15d55306fa2b2c19908cf1d310b2
Vasilka69 <114226684+Vasilka69@users.noreply.github.com>
2023-01-10 18:19:59 +0500

    Update main.py

--- 1 лаба LZW/Laba1/main.py
+++ 1 лаба LZW/Laba1/main.py
@@ -8,16 +8,16 @@ def printm2d(mas):
     print('---')
     for i in range(len(mas[0])):
         for j in range(len(mas)):
-            if i != 0 and len(str(mas[j][i])) < 4:
-                for _ in range(j-1):
-                    print('', end='\t')
-
-            if i != 0  and j > 2 and len(str(mas[j][i])) >= 4:
-                    print('', end='\t')
-            if i != 0  and j > 3 and len(str(mas[j][i])) >= 4:
-                    print('', end='\t')
-
             try:
+                if i != 0 and len(str(mas[j][i])) < 4:
+                    for _ in range(j-1):
+                        print('', end='\t')
+
+                if i != 0  and j > 2 and len(str(mas[j][i])) >= 4:
+                        print('', end='\t')
+                if i != 0  and j > 3 and len(str(mas[j][i])) >= 4:
+                        print('', end='\t')
+                
                 print(mas[j][i], end='\t\t')
             except:
                 print(' ', end='')
@@ -45,6 +45,7 @@ def encoder(text):
         #print(f'N = {N}')
         table[0].append(c)
         temp = text[N]
+        #temp = ''
         l = 1
         while dictionary.__contains__(temp): # Переписать через фор
             if N + l < n:
@@ -54,10 +55,11 @@ def encoder(text):
                 break
         dictionary.append(temp)
         table[1].append(temp)
+        '''
         table[2].append(temp)
         table[3].append(temp)
         table[4].append(temp)
-        #l = len(temp)
+        '''
         if l != 1:
             l -= 1
         N += l
